260e682a821b01c883f15dcbf9597824f16ed4a8
刘子毓 <1754644738@qq.com>
2020-09-28 18:44:54 +0800

    CORS配置

---
+++ src/main/java/com/example/mybatis/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.mybatis;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.time.Duration;
+
+@Configuration
+public class CorsConfig  {
+
+    @Bean
+    public FilterRegistrationBean corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        //设置你要允许的网络域名，如果全允许则设为*
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(Duration.ofDays(5));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
